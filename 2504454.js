8967c1940272842d96433ae69edd4727fd43b67a
William <wawesterlund@gmail.com>
2026-03-25 01:00:27 +0200

    2nd commit, without folder

---
+++ 2504454.js
@@ -0,0 +1,38 @@
+const reader = new Html5Qrcode("camera");
+let scannerOn = false;
+
+function toggleScanner() {
+    scannerOn = !scannerOn;
+    if (scannerOn) {
+        startScanner();
+        mapContainer.style.display ="none";
+        btn.innerText = "CANCEL";
+    } else {
+        stopScanner();
+        mapContainer.style.display ="block";
+        btn.innerText ="SCAN";
+    }
+}
+
+function startScanner() {
+    reader.start(
+        { facingMode: "environment" },
+        {},
+        function (text) {
+            const place = JSON.parse(text);
+            showMarkerAt(place.top, place.left);
+            toggleScanner();
+        }
+    ).catch(function (err) {
+        console.error(err);
+    });
+}
+
+function stopScanner() {
+    reader.stop();
+}
+
+function showMarkerAt(top, left) {
+    marker.style.top = top;
+    marker.style.left = left;
+}
